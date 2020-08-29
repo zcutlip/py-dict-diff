3cc92959f644685199877ff1dfdecc028589f432
Zachary Cutlip <uid000@gmail.com>
2020-08-29 11:20:36 -0700

    added version/about code

--- dictdiff/__init__.py
+++ dictdiff/__init__.py
@@ -1,1 +1,13 @@
 from .dictdiff import DictDiff  # noqa: F401
+
+from .__about__ import (
+    __version__,
+    __title__,
+    __summary__
+)
+
+__all__ = [
+    "__version__",
+    "__title__",
+    "__summary__"
+]
